307ed0c0feff4925cb4ec25d23e9e93e88cfd7e5
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-11 14:15:10 -0500

    done

--- Game.cs
+++ Game.cs
@@ -8,11 +8,13 @@ namespace Constructors_Jackson_Lydia
 {
     internal class Game
     {
-
+        // two public string fields one for title and one for genre
         public string title, genre;
 
+        // a constructor with two string parameters, with default values
         public Game(string titleParam = "Unknown Title", string genreParam = "Casual")
         { 
+            // sets the value for title and genre to titleParam and genreParam
             title = titleParam;
             genre = genreParam;
         }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //a game object called myGame with the value of my favorite game and its genre
+            Game myGame = new Game("Borderlands", "Science Fiction FPS");
+
+            //prints to console the title and genre of the game object myGame
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game!");
+
+            //a game object called myOtherGame
+            Game myOtherGame = new Game();
+
+            //prints to console the title and genre of the game object myOtherGame
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is rated {myOtherGame.genre}!");
         }
     }
 }
